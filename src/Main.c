8dfd2af4a65e7bf938eeceb2cb5930c2bb042a71
codeleaded <gnu.c.reaper@gmail.com>
2026-03-02 17:29:39 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,10 +1,17 @@
-#include "/home/codeleaded/System/Static/Library/DLS.h"
+#include "/home/codeleaded/System/Static/Library/ChipSet.h"
 
 int main(){
-    
-    DLS dls = DLS_New();
-    
-    DLS_Free(&dls);
+    ChipSet cs = ChipSet_Make_Std();
+    Chip_Signal outs[] = { 0x0ULL };
 
+    ChipSet_Exe(
+        &cs,
+        "AND",
+        (Chip_Signal[]){ 0x1ULL,0x1ULL },
+        outs
+    );
+    printf("Out: %llu\n",outs[0]);
+
+    ChipSet_Free(&cs);
     return 0;
 }
